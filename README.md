042d88965e175edb4e70239245b9fdeadebb7564
iqbalbash <id.iqbalbash@gmail.com>
2018-04-23 16:30:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ovcp-geteasy
---
+++ VestaCP/apache.sh
@@ -0,0 +1,5 @@
+# Download installation script
+curl -O http://vestacp.com/pub/vst-install.sh
+
+# Installation Apache
+bash vst-install.sh --nginx no --apache yes --phpfpm no --named yes --remi yes --vsftpd no --proftpd yes --iptables yes --fail2ban yes --quota yes --exim yes --dovecot yes --spamassassin yes --clamav yes --softaculous no --mysql yes --postgresql no
